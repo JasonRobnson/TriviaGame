eb22f37b54d1deb525d721bab2f988aab5f71dd3
unknown <bubbastinx@gmail.com>
2018-03-14 14:52:23 -0600

    created spot where timer data will be displayed

---
+++ Assets/javascript/app.js
@@ -0,0 +1,30 @@
+'strict'
+
+// let triviaObj = {
+//     triviaKey: [["Question1","Answers1", "Gif1"], ["Question2", "Answer2", "Gif2"], ["Question3", "Answer3", "Gif3"]],
+//     displayQuest: ,
+//     startPage: ,
+//     correctAnswer: ,
+//     wrongAnswer: ,
+//     nextQuestion: ,
+//     timeoutTimer:  function timeOut() {
+//         noCanvas() ;
+//         let write = document.getElementById("timerDiv");
+//         timeDiv.html("this is where the timer will go!")
+
+
+//     },
+//     finalScreen: .
+
+// };
+
+let write = document.getElementById("timerDiv");
+
+function timeOut() {
+    let write = document.getElementById("#timerDiv")
+    timerDiv.innerHTML = ("<b>this is where the timer will go!</b>");
+};
+
+
+timeOut();
+

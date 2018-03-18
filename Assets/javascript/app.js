124a387101b0f01d65a3cc29238da6a6a92b52d9
unknown <bubbastinx@gmail.com>
2018-03-17 20:46:41 -0600

    getting started working on redoing the start button

--- Assets/javascript/app.js
+++ Assets/javascript/app.js
@@ -1,38 +1,70 @@
 'strict'
-const trivia = {
+const trivia = [{
     questions: "This item dominated the Hip-Hop and Emo seen. It could be found at your local mall, but now relegated to your local sports store?",
-    answer: ["Cartiers  ", "Timberlands ", "Flannel Jackets ", "Sweatbands  ",],
-    correctAnswer:"Sweatbands", 
+    answer: ["Cartiers  ", "Timberlands ", "Flannel Jackets ", "Sweatbands  ", ],
+    correctAnswer: "Sweatbands",
     // image:?????????????????????????,
-};
+},
+{
+    questions: "This item dominated the Hip-Hop and Emo seen. It could be found at your local mall, but now relegated to your local sports store?",
+    answer: ["Cartiers  ", "Timberlands ", "Flannel Jackets ", "Sweatbands  ", ],
+    correctAnswer: "Sweatbands",
+    // image:?????????????????????????,
+},
+];
 
 var triviaQuestion = trivia.questions;
 var triviaAnswer = trivia.answer;
 
 function startButtonHandler() {
-$("#buttonStart").click(function() {
-    loadQuestion();
-    setTimeout(loadAnswer,(2000));
-    // startTimer();
-})
+    $("#buttonStart").click(function () {
+        loadQuestion();
+        setTimeout(loadAnswer, (1500));
+        // $("#allbuttons").hide();
+        // startTimer();
+    })
 };
 
 
 function startTimer() {
-    
-    setTimeout(function(){ alert("Start Trivia button has been pushed!"); }, 300);
-};
-function loadQuestion (){
-     $("#questionDiv").text();
-$("#questionDiv").html(triviaQuestion);
+
+    setTimeout(function () {
+        alert("Start Trivia button has been pushed!");}, 3000);
 };
 
-function loadAnswer(){
-    $("#answerDiv").text();
-    $("#answerDiv").prepend("<button id=\"buttonA\" type=\"button\">"+ trivia.answer[0] + "</button><buttonid=\"buttonB\" type=\"button\">" +  trivia.answer[1] + "</button><button id=\"buttonC\" type=\"button\">"+ trivia.answer[2] + "</button><button id=\"buttonD\" type=\"button\">" +  trivia.answer[3] + "</button>"); 
-
-    
+function loadQuestion() {
+    $("#questionDiv").text();
+    $("#questionDiv").html(trivia[0].questions);
 };
+
+function loadAnswer() {
+
+   for ( let x = 0; x < trivia[0].answer.length; x++) {
+       let answerButton = $("<button>");
+        answerButton.addClass("answerKeys");
+        answerButton.attr("data-answerButton", trivia[0].answer[x]);
+        answerButton.append(trivia[0].answer[x]);
+        $("#allbuttons").append(answerButton);
+    }
+    // $("#allbuttons").prepend("<button id=\"buttonA\"></button><button id=\"buttonB\"></button><button id=\"buttonC\"></button><button id=\"buttonD\"></button>");
+    // // for loop
+    // $("#buttonA").text(trivia.answer[0]);
+    // $("#buttonB").text(trivia.answer[1]);
+    // $("#buttonC").text(trivia.answer[2]);
+    // $("#buttonD").text(trivia.answer[3]);
+    // // $("#answerDiv").prepend("<button id=\"buttonA\" type=\"button\">" + trivia.answer[0] + "</button><buttonid=\"buttonB\" type=\"button\">" + trivia.answer[1] + "</button><button id=\"buttonC\" type=\"button\">" + trivia.answer[2] + "</button><button id=\"buttonD\" type=\"button\">" + trivia.answer[3] + "</button>");
+};
+
+function answerButton() {
+    $("#buttonA").click(function () {
+    console.log("#buttonA");
+    })
+};
+$("#allbuttons").on('click','.answerKeys',function(){
+    $(this).attr("data-answerButton");
+    console.log($(this).attr("data-answerButton"));
+})
+
 startButtonHandler();
 
 
@@ -74,66 +106,66 @@ startButtonHandler();
 //     reset: function(){
 
 //     },
-   
+
 // }
 
 
-    // },{
-    // question: ,
-    // answer: [],
-    // correctAnswer:"Streaky Hair Highlights ",""
-    // image: ??????????????,
-    // },{
-    // question: ,
-    // answer: [],
-    // correctAnswer:"Tall Tees" ,
-    // image: ??????????????,
-    // },{
-    // question: ,
-    // answer: [],
-    // correctAnswer:"Trucker Hats",
-    // image: ??????????????,
-    // },{
-    // question: ,
-    // answer: [],
-    // correctAnswer:"Livestrong Bracelets" ,
-    // image: ??????????????,
-    // },{
-    // question: ,
-    // answer: [],
-    // correctAnswer:"Popped Collar Polos" ,
-    // image: ??????????????,
-    // },{
-    // question: ,
-    // answer: [],
-    // correctAnswer:"Fedoras" ,
-    // image: ??????????????,
-    // },{
-    // question: ,
-    // answer: [],
-    // correctAnswer:"V-Neck T-shirts" ,
-    // image: ??????????????,
-    // },{
-    // question: ,
-    // answer: [],
-    // correctAnswer:"Shutter Shades" ,
-    // image: ??????????????,
-    // },{
-    // question: ,
-    // answer: [],
-    // correctAnswer:"Throwback Jerseys" ,
-    // image: ??????????????,
-    // },{
-    // question: ,
-    // answer: [],
-    // correctAnswer:"Bootcut Jeans" ,
-    // image: ??????????????,
-    // },{
-    // question: ,
-    // answer: [],
-    // correctAnswer:"Emo/Bieber Hair" ,
-    // image: ??????????????,
-    // }
+// },{
+// question: ,
+// answer: [],
+// correctAnswer:"Streaky Hair Highlights ",""
+// image: ??????????????,
+// },{
+// question: ,
+// answer: [],
+// correctAnswer:"Tall Tees" ,
+// image: ??????????????,
+// },{
+// question: ,
+// answer: [],
+// correctAnswer:"Trucker Hats",
+// image: ??????????????,
+// },{
+// question: ,
+// answer: [],
+// correctAnswer:"Livestrong Bracelets" ,
+// image: ??????????????,
+// },{
+// question: ,
+// answer: [],
+// correctAnswer:"Popped Collar Polos" ,
+// image: ??????????????,
+// },{
+// question: ,
+// answer: [],
+// correctAnswer:"Fedoras" ,
+// image: ??????????????,
+// },{
+// question: ,
+// answer: [],
+// correctAnswer:"V-Neck T-shirts" ,
+// image: ??????????????,
+// },{
+// question: ,
+// answer: [],
+// correctAnswer:"Shutter Shades" ,
+// image: ??????????????,
+// },{
+// question: ,
+// answer: [],
+// correctAnswer:"Throwback Jerseys" ,
+// image: ??????????????,
+// },{
+// question: ,
+// answer: [],
+// correctAnswer:"Bootcut Jeans" ,
+// image: ??????????????,
+// },{
+// question: ,
+// answer: [],
+// correctAnswer:"Emo/Bieber Hair" ,
+// image: ??????????????,
+// }
 // }];
 
 // startTimer()};

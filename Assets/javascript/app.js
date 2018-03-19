5dfa86f0305e1968a1a84e2fc67339f961f9a8cc
unknown <bubbastinx@gmail.com>
2018-03-19 12:02:06 -0600

    did a revision of previous push, added all functions to Objects as method, Answer buttons and question generated

--- Assets/javascript/app.js
+++ Assets/javascript/app.js
@@ -1,25 +1,22 @@
 'strict'
 
-$(document).ready(function() {
-    startButtonHandler();
-});
-
 // $(document).on('click','.answerKeys', function(){
 //     trivia.clicked();
 // })
 
-const trivia = [{
-    questions: "This item dominated the Hip-Hop and Emo scene. It could be found at your local mall, but now relegated to your local sports store?",
+
+
+let triviaQuestions = [{
+    question: "This item dominated the Hip-Hop and Emo scene. It could be found at your local mall, but now relegated to your local sports store?",
     answer: ["Cartiers  ", "Timberlands ", "Flannel Jackets ", "Sweatbands  ", ],
     correctAnswer: "Sweatbands",
     image: "./Assets/Images/headBands.jpg",
-},
-{
-    questions: "These wearable items were cheap, colorful, and fit with any fashion style.",
+    },{
+    question: "These wearable items were cheap, colorful, and fit with any fashion style.",
     answer: ["Plastic Watches", "Drawstring Backpack", "Blank Sweatshirts", "Grills", ],
     correctAnswer: "Plastic Watches",
     image: "./Assets/Images/plasticWatches.jpg",
-},{
+    },{
     question: "Which hairstyle was introduce during the 2000's by soccer God David Beckham?",
     answer: ["Mullet", "Bowl Cut", "Faux Hawk", "Buzz Cut",],
     correctAnswer:"Faux Hawk",
@@ -57,138 +54,99 @@ const trivia = [{
     }];
 
 
-let triviaQuestion = trivia.questions;
-let triviaAnswer = trivia[0].answer;
-let triviaCorrect = trivia[0].correctAnswer;
-let counter = 5;
-let timer;
+    let game = {
+         triviaQuestions: triviaQuestions,
+         currentQuestion: 0,
+         counter: 5,
+         correct: 0,
+         incorrect: 0,
 
-
-function startButtonHandler() {
-    $("#buttonStart").html();
-    let startButton = $("<button>");
-    startButton.addClass("startButton");
-    startButton.text("Start");
-    $("#buttonStart").append(startButton);
-    $("#buttonStart").on('click',function (){
-        $(this).hide();
-        loadQuestion();
-        gameTimer();
-        loadAnswer();
-    })
-};
-
-
-function gameTimer() {
-    //  $("#timerDiv").html( counter);
-      let timer = setInterval(function() {
-            $("#timerDiv").html("<h6>You have " + counter + " seconds reaming...");
-            if (counter === 0) {
-                timeOver();
-            } else {
-                counter--;
-            }
-        },1000);
-    };
-
-
-function timeOver() {
-    clearInterval(timer);
-    $("#timerDiv").html('<h2>Out of Time!</h2>');
-
-}
-
-
-function loadQuestion() {
-    $("#questionDiv").text();
-    $("#questionDiv").html('<h3>' + trivia[0].questions);
-    
-};
-
-function loadAnswer() {
-
-   for ( let x = 0; x < trivia[0].answer.length; x++) {
+    startButton: function startButtonHandler() {
+        $("#buttonStart").html();
+        let startButton = $("<button>");
+        startButton.addClass("startButton");
+        startButton.text("Start");
+        $("#buttonStart").append(startButton);
+        $("#buttonStart").on('click',function (){
+            $(this).hide();
+            loadQuestion();
+            gameTimer();
+            loadAnswer();
+        })
+    },
+    gameTimer: function gameTimer() {
+        //  $("#timerDiv").html( counter);
+          let timer = setInterval(function() {
+                $("#timerDiv").html("<h6>You have " + counter + " seconds reaming...");
+                if (counter === 0) {
+                    // timeOver();
+                } else {
+                    counter--;
+                }
+            },1000);
+        },
+    timeOver: function timeOver() {
+    //     $("#timerDiv").html('<h2>Sorry! You are out of time!</h2>');
+    //     clearInterval(timer);
+    },
+    loadQuestion: function loadQuestion() {
+        counter = setInterval(game.gameTimer,1000);    
+        $("#questionDiv").html('<h3>' + triviaQuestions[game.currentQuestion].question + '</h3>');
+        // for(let i = 0; i <question[game.currentQuestion].answer.Length; i++)
+    },
+    loadAnswer: function loadAnswer() {
+        for ( let x = 0; x < triviaQuestions[game.currentQuestion].answer.length; x++) {
        let answerButton = $("<button>");
-        answerButton.addClass("answerKeys");
-        answerButton.attr("data-answerButton", trivia[0].answer[x]);
-        answerButton.append(trivia[0].answer[x]);
-        $("#allbuttons").append(answerButton);
-    }
+       answerButton.addClass("answerKeys");
+       answerButton.attr("data-answerButton", triviaQuestions[game.currentQuestion].answer[x]);
+       answerButton.append(triviaQuestions[game.currentQuestion].answer[x]);
+       $("#allbuttons").append(answerButton);
+     }
+    }, 
+    answerSelection: function answerSelection() {
+        clearInterval();
+        if($(e.targert).data("name")=== triviaCorrect) {
+            correctAnswer();
+        } else {
+            wrongAnswer();
+        }
+    },
+    correctAnswer: function correctAnswer() {
+        console.log ("You won!")
+    },
+    wrongAnswer: function wrongAnswer() {
+        console.log("You lost!")
+    },
+    results: function results() {
+
+    },  
+    correctTally: function() {
+
+    },
+    incorrectTally: function(){
+
+    },
+    reset: function() {
+
+    },
 };
 
-$("#allbuttons").on('click','.answerKeys',function(){
-    $(this).attr("data-answerButton");
-    console.log($(this).attr("data-answerButton"));
-    console.log(triviaCorrect);
-    let x = $(this).attr("data-answerButton");
-    console.log(x);
-    if (x == triviaCorrect) {
-        console.log("You Won!")
-    } else {
-        console.log("You Lose!")
-    }
+$(document).ready(function() {
+    game.loadQuestion();
+    game.loadAnswer();
 });
 
-function answerSelection() {
-    clearInterval();
-    if($(e.targert).data("name")=== triviaCorrect) {
-        correctAnswer();
-    } else {
-        wrongAnswer();
-    }
-};
 
-function correctAnswer() {
-        console.log ("You won!")
-};
+// $("#allbuttons").on('click','.answerKeys',function(){
+//     $(this).attr("data-answerButton");
+//     console.log($(this).attr("data-answerButton"));
+//     console.log(triviaCorrect);
+//     let x = $(this).attr("data-answerButton");
+//     console.log(x);
+//     if (x == triviaCorrect) {
+//         console.log("You Won!")
+//     } else {
+//         console.log("You Lose!")
+//     }
+// });
 
-function wrongAnswer() {
-        console.log("You lost!")
-};
-
-
-// var game = {
-//     question: ["questions"],
-//     currentQuestion:0,
-//     timer: 30,
-//     correct: 0,
-//     incorrect: 0,
-//     countdown: function () {
-//         game.timer--;
-//         $('#timerDiv').html(game.counter);
-//         if(game.counter<=0){
-//              console.log("Time UP!");
-//         }
-
-//     },
-//     loadQuestion: function (){
-
-//     },
-//     nextQuestion: function(){
-
-//     },
-//     timeUp: function(){
-
-//     },
-//     result: function(){
-
-//     },
-//     clicked: function(){
-
-//     },
-//     answeredCorrectly: function (){
-
-//     },
-//     answeredIncorrectly: function(){
-
-//     },
-//     reset: function(){
-
-//     },
-
-// }
-
-
-// }
-
-// startTimer()};
